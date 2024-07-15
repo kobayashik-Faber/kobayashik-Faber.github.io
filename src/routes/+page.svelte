997df0b343ff6da988e2e0150bbddba2a63a3665
Kota Kobayashi <kota.kobayashi@fabercompany.co.jp>
2024-07-15 15:12:48 +0900

    :mag: Add Mieruca Optimize Tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,77 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+  import Counter from "./Counter.svelte";
+  import welcome from "$lib/images/svelte-welcome.webp";
+  import welcome_fallback from "$lib/images/svelte-welcome.png";
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
+  <!-- Mieruca Optimize Tag -->
+  <script type="text/javascript" id="mierucaOptimizejs">
+    window.__optimizeid = window.__optimizeid || [];
+    __optimizeid.push([1150133615]);
+    (function () {
+      var fjs = document.createElement("script");
+      fjs.type = "text/javascript";
+      fjs.async = true;
+      fjs.id = "fjssync";
+      var timestamp = new Date();
+      fjs.src =
+        "https://dev.opt.mieru-ca.com/service/js/mieruca-optimize-dev.js?v=" +
+        timestamp.getTime();
+      var x = document.getElementsByTagName("script")[0];
+      x.parentNode.insertBefore(fjs, x);
+    })();
+  </script>
+  <!-- End Mieruca Optimize Tag -->
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
+  <h1>
+    <span class="welcome">
+      <picture>
+        <source srcset={welcome} type="image/webp" />
+        <img src={welcome_fallback} alt="Welcome" />
+      </picture>
+    </span>
 
-		to your new<br />SvelteKit app
-	</h1>
+    to your new<br />SvelteKit app
+  </h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
+  <h2>
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
 
-	<Counter />
+  <Counter />
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
 
-	h1 {
-		width: 100%;
-	}
+  h1 {
+    width: 100%;
+  }
 
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
+  .welcome {
+    display: block;
+    position: relative;
+    width: 100%;
+    height: 0;
+    padding: 0 0 calc(100% * 495 / 2048) 0;
+  }
 
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  .welcome img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    display: block;
+  }
 </style>
