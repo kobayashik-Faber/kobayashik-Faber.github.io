c0f83f5a7bc796f92bf4f8f1924dbb7e66de630e
Kota Kobayashi <kota.kobayashi@fabercompany.co.jp>
2025-01-12 23:02:11 +0900

    ➕ Add rollup-plugin-visualizer

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -1,16 +1,41 @@
+import type { UserConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-export default defineConfig({
-  plugins: [sveltekit()],
+export default defineConfig(({ mode }) => {
+  let config: UserConfig = {
+    plugins: [sveltekit()],
 
-  test: {
-    include: ['src/**/*.{test,spec}.{js,ts}'],
-  },
-
-  server: {
-    fs: {
-      allow: ['styled-system'],
+    test: {
+      include: ['src/**/*.{test,spec}.{js,ts}'],
     },
-  },
+
+    server: {
+      fs: {
+        allow: ['styled-system'],
+      },
+    },
+  };
+
+  switch (mode) {
+    case 'analyze':
+      {
+        config = {
+          ...config,
+          plugins: config.plugins?.concat([
+            visualizer({
+              emitFile: true,
+              filename: 'stats.html',
+            }),
+          ]),
+          build: {
+            minify: false,
+          },
+        };
+      }
+      break;
+  }
+
+  return config;
 });
